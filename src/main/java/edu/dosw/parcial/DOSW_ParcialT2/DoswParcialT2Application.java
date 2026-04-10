05ffb3b2f2f9b4a47ca864587f945fb2e64f6afe
Juan Melo <juan.melo-s@mail.escuelaing.edu.co>
2026-04-09 22:52:35 -0500

    feat: initial project setup with dependencies and structure

---
+++ src/main/java/edu/dosw/parcial/DOSW_ParcialT2/DoswParcialT2Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.DOSW_ParcialT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswParcialT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswParcialT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/parcial/DOSW_ParcialT2/DoswParcialT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial.DOSW_ParcialT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswParcialT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
